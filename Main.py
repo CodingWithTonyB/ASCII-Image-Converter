fe468c74b4612273a8ee0d7a11a32b026a19c46f
C0dingWithT0ny <78626437+C0dingWithT0ny@users.noreply.github.com>
2023-02-14 16:40:32 -0800

    Original

---
+++ Main.py
@@ -0,0 +1,49 @@
+from PIL import Image, ImageDraw, ImageFont
+import math
+
+path = r"C:\Users\Crypt\Desktop\Pictures\photo_2023-02-14_13-36-21.jpg"
+
+chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
+# chars = "#Wo- "[::-1]
+charArray = list(chars)
+charLength = len(charArray)
+interval = charLength/256
+
+scaleFactor = 0.0001
+
+#0 is regular for brightness 
+# adding brightness just increases 
+# the r g b by the ammount
+brightness = 30
+
+oneCharWidth = 10
+oneCharHeight = 16
+
+def getChar(inputInt):
+    return charArray[math.floor(inputInt*interval)]
+
+text_file = open("Output.txt", "w")
+
+im = Image.open(path)
+
+fnt = ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf', 15)
+
+width, height = im.size
+im = im.resize((int(scaleFactor*width), int(scaleFactor*height*(oneCharWidth/oneCharHeight))), Image.NEAREST)
+width, height = im.size
+pix = im.load()
+
+outputImage = Image.new('RGB', (oneCharWidth * width, oneCharHeight * height), color = (0, 0, 0))
+d = ImageDraw.Draw(outputImage)
+
+for i in range(height):
+    for j in range(width):
+        r, g, b = pix[j, i]
+        h = int(r/3 + g/3 + b/3)
+        pix[j, i] = (h, h, h)
+        text_file.write(getChar(h))
+        d.text((j*oneCharWidth, i*oneCharHeight), getChar(h), font = fnt, fill = (r+brightness, g+brightness, b+brightness))
+
+    text_file.write('\n')
+
+outputImage.save('ASCII\\ASCII Image Converter\\output.png')
